2a61504bb0f19ec603676523f4cfacd36b25c05e
Felipe <aloferreirafm@gmail.com>
2023-06-13 18:00:52 -0300

    hoje

--- Pages/Login/Index.js
+++ Pages/Login/Index.js
@@ -8,8 +8,7 @@ import {
 import { Feather } from '@expo/vector-icons';
 import { useNavigation } from '@react-navigation/core';
 import api from '../../ApiService/api';
-import * as Device from 'expo-device';
-import * as Notifications from 'expo-notifications';
+
 import MyButton  from '../../Components/MyButton/Index';
 import LinkButton from '../../Components/LinkButton/Index';
 
@@ -18,64 +17,18 @@ import Loading from '../../Components/Loading/index';
 
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
+import { Formik } from 'formik';
+import * as yup from 'yup';
+
+const validationSchema = yup.object().shape({
+    login: yup.string().required('Login é obrigatório'),
+    password: yup.string().min(8, 'Senha deve conter no mínimo 8 caracteres').required('Senha é obrigatória'),
+  });
+
 const eye = 'eye';
 const eyeOff = 'eye-off';
 
-Notifications.setNotificationHandler({
-    handleNotification: async () => ({
-      shouldShowAlert: true,
-      shouldPlaySound: true,
-      shouldSetBadge: true,
-    }),
-  });
-  
-    function NotificationTest() { 
-    const [expoPushToken, setExpoPushToken] = useState('');
-    const [notification, setNotification] = useState(false);
-    const notificationListener = useRef();
-    const responseListener = useRef();
-  
-    useEffect(() => {
-      registerForPushNotificationsAsync().then(token => setExpoPushToken(token));
-  
-      notificationListener.current = Notifications.addNotificationReceivedListener(notification => {
-        setNotification(notification);
-      });
-  
-      responseListener.current = Notifications.addNotificationResponseReceivedListener(response => {
-        console.log(response);
-      });
-  
-      return () => {
-        Notifications.removeNotificationSubscription(notificationListener.current);
-        Notifications.removeNotificationSubscription(responseListener.current);
-      };
-    }, []);
-  
-    return (
-      <View
-        style={{
-          flex: 1,
-          alignItems: 'center',
-          justifyContent: 'space-around',
-        }}>
-        <Text>Sua expo push token: {expoPushToken}</Text>
-        <View style={{ alignItems: 'center', justifyContent: 'center' }}>
-          <Text>Title: {notification && notification.request.content.title} </Text>
-          <Text>Body: {notification && notification.request.content.body}</Text>
-          <Text>Data: {notification && JSON.stringify(notification.request.content.data)}</Text>
-        </View>
-        <Button
-          title="Notificação Local"
-          onPress={async () => {
-            await schedulePushNotification();
-          }}
-        />
-    </View>
-  );
-}
-
-export default function Login() {
+export default async function Login() {
 
 const [flShowPass, setShowPass] =  useState(false);
 const [iconPass, setIconPass] =  useState(eye);
@@ -83,40 +36,116 @@ const [txtLogin, setLogin] = useState('')
 const [txtSenha, setSenha] = useState('')
 const navigation = useNavigation();
 const [flLoading, setLoading] = useState(false)
+
+const [expoPushToken, setExpoPushToken] = useState('');
+const [notification, setNotification] = useState(false);
+const notificationListener = useRef();
+const responseListener = useRef();
+
+
 let number = 0
 
 function handleChangeIcon() {
+  let icone = iconPass == eye ? eyeOff : eye;
+  let flShowPassAux = !flShowPass;
+  setShowPass(flShowPassAux);
+  setIconPass(icone);
+}
+
+notification.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: true,
+    shouldSetBadge: true,
+  }),
+});
+
+
+  useEffect(() => {
+    registerForPushNotificationsAsync().then(token => setExpoPushToken(token));
+
+    notificationListener.current = notification.addNotificationReceivedListener(notification => {
+      setNotification(notification);
+    });
+
+    responseListener.current = notification.addNotificationResponseReceivedListener(response => {
+      console.log(response);
+    });
+
+    return () => {
+      notification.removeNotificationSubscription(notificationListener.current);
+      notification.removeNotificationSubscription(responseListener.current);
+    };
+  }, []);
+/*
+  return (
+    <View
+      style={{
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'space-around',
+      }}>
+      <Text>Sua expo push token: {expoPushToken}</Text>
+      <View style={{ alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Title: {notification && notification.request.content.title} </Text>
+        <Text>Body: {notification && notification.request.content.body}</Text>
+        <Text>Data: {notification && JSON.stringify(notification.request.content.data)}</Text>
+      </View>
+      <Button
+        title="Notificação Local"
+        onPress={async () => {
+          await schedulePushNotification();
+        }}
+      />
+  </View>
+);
+}
+*/
+async function resetSenha(){
+  number.random(/^(?=.*[a-z])(?=.*[A-Z]){4}$/)
+  await notification.scheduleNotificationAsync({
+      content: {
+        title: "Mudança de senha!",
+        body: `Você solicitou uma alteração na sua senha. Para realiza-lá, insira o seguinte código no campo exigido. ${number}`,
+        data: { data: 'goes here' },
+      },
+      trigger: { seconds: 2 },
+    });
+}
+
+const handleLogin = async (values) => {
+        setLoading(true);
+        let resposta = 0;
+        
+        await api.get(`/Users`).then((response) => {
+        resposta = response.data.length;
+        
+        if(resposta == 0){
+            alert('Usuario e/ou senha inválido!');
+            setLoading(false);
+            return;
+        } else {
+      
+            AsyncStorage.setItem('@nameApp:userName', txtLogin);      
+            navigation.navigate('HomeMenu');   
+            setLoading(false);
+    
+        }
+    
+ 
+     }).catch(err => alert(err));
+
+}
+
+
+async function handleChangeIcon() {
     let icone = iconPass == eye ? eyeOff : eye;
     let flShowPassAux = !flShowPass;
     setShowPass(flShowPassAux);
     setIconPass(icone);
 }
 
-async function resetSenha(){
-    number.random(/^(?=.*[a-z])(?=.*[A-Z]){4}$/)
-    await Notifications.scheduleNotificationAsync({
-        content: {
-          title: "Mudança de senha!",
-          body: `Você solicitou uma alteração na sua senha. Para realiza-lá, insira o seguinte código no campo exigido. ${number}`,
-          data: { data: 'goes here' },
-        },
-        trigger: { seconds: 2 },
-      });
-}
-
-async function navigateToHome() {
-    setLoading(true);
-    if (txtLogin.trim() === '') {
-        alert('Campo login é obrigatório');
-        setLoading(false);
-        return;
-    }
-    if (txtSenha.trim() === '') {
-        alert('Campo senha é obrigatório');
-        setLoading(false);
-        return;
-    }
-
+/*
     let resposta = 0;
     await api.get(`/Users`).then((response) => {
         //setJokesList(response.data);
@@ -132,43 +161,38 @@ async function navigateToHome() {
             navigation.navigate('HomeMenu');   
             setLoading(false);
     
-        }
-    
- 
+        } 
      }).catch(err => alert(err));
+    }
+*/
+  return (
+    <Formik
+      initialValues={{ login: '', password: '' }}
+      validationSchema={validationSchema}
+      onSubmit={handleLogin}
+    >
+     {({ handleChange, handleBlur, handleSubmit, values, errors, touched }) => (
 
-  /*  wait api.get(`/Users?name=${txtLogin}&password=${txtSenha}`).then((response) => {
-        //setJokesList(response.data);
-        resposta = response.data.length;
-        alert(response.data.length);
-     }).catch(err => alert(err));*/
-
-   
-}
-
-function navigateToNewUser() {
-    navigation.navigate('NewUser');
-}
-if (flLoading) {
-    return (<Loading />);
-}
-
-return (
-    <View style={styles.container}>
+        <View style={styles.container}>
         <Text style={styles.textTitle}>Seja bem vindo!</Text>
+
+
         <TextInput
             style={styles.textInput}
             placeholder="Login"
-            onChangeText={text => setLogin(text)}
-            value={txtLogin}
-        
+            onChangeText={handleChange('login')}
+            value={values.login}
+            onBlur={handleBlur('login')}
         />
+        {touched.login && errors.login && <Text style={styles.errorText}>{errors.login}</Text>}
+
         <View style={styles.passwordContainer}>
             <TextInput
                 style={styles.textInputPassword}
                 placeholder="Senha"
-                onChangeText={text => setSenha(text)}
-                value={txtSenha}
+                onChangeText={handleChange('password')}
+                onBlur={handleBlur('password')}
+                value={values.password}
                 secureTextEntry={flShowPass}
             />
             <Feather
@@ -179,22 +203,20 @@ return (
                 onPress={handleChangeIcon}
             />
         </View>
-
-        <MyButton title='Entrar' onPress={navigateToHome}
-
-        />
-
+        {touched.password && errors.password && <Text style={styles.errorText}>{errors.password}</Text>}
+        <MyButton title='Entrar' onPress={handleSubmit}     />
+     
         <LinkButton title='Inscrever-se'
             onPress={navigateToNewUser}
         />
-        <notaTeste title= 'Resetar Senha' onPress={resetSenha}
-        />
+        <MyButton title= 'Resetar Senha' onPress={resetSenha}/>
 
     </View>
+   )}
+   </Formik>
 
-);
-}
-
+  );
+  }
 
 const styles = StyleSheet.create({
 container: {
@@ -247,8 +269,12 @@ passwordContainer: {
     flexDirection: 'row',
     justifyContent: 'space-between'
 },
+errorText: {
+    color: 'red',
+    marginBottom: 10,
+  },
 iconEye: {
     paddingHorizontal: 8,
     marginTop: 6
-},
+}
 });
---
+++ Pages/SenhaNova/Index.js
@@ -0,0 +1,195 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
+import React, {useState} from 'react';
+import { useNavigation } from '@react-navigation/native'
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import colors from '../../styles/colors';
+import * as Device from 'expo-device';
+import * as Notifications from 'expo-notifications';
+import MyButton from '../../Components/MyButton/Index';
+
+const validationSchema = yup.object().shape({
+    login: yup.string().required('Login é obrigatório'),
+    password: yup.string().min(8, 'Senha deve conter no mínimo 8 caracteres').required('Senha é obrigatória'),
+  });
+
+export default function SenhaNova() {
+    const navigation = useNavigation();
+    const [txtcodigo, setcodigo] = useState('')
+    let number = 0
+
+
+Notifications.setNotificationHandler({
+    handleNotification: async () => ({
+      shouldShowAlert: true,
+      shouldPlaySound: true,
+      shouldSetBadge: true,
+    }),
+  });
+  
+    function NotificationTest() { 
+    const [expoPushToken, setExpoPushToken] = useState('');
+    const [notification, setNotification] = useState(false);
+    const notificationListener = useRef();
+    const responseListener = useRef();
+  
+    useEffect(() => {
+      registerForPushNotificationsAsync().then(token => setExpoPushToken(token));
+  
+      notificationListener.current = Notifications.addNotificationReceivedListener(notification => {
+        setNotification(notification);
+      });
+  
+      responseListener.current = Notifications.addNotificationResponseReceivedListener(response => {
+        console.log(response);
+      });
+  
+      return () => {
+        Notifications.removeNotificationSubscription(notificationListener.current);
+        Notifications.removeNotificationSubscription(responseListener.current);
+      };
+    }, []);
+  
+    return (
+      <View
+        style={{
+          flex: 1,
+          alignItems: 'center',
+          justifyContent: 'space-around',
+        }}>
+        <Text>Sua expo push token: {expoPushToken}</Text>
+        <View style={{ alignItems: 'center', justifyContent: 'center' }}>
+          <Text>Title: {notification && notification.request.content.title} </Text>
+          <Text>Body: {notification && notification.request.content.body}</Text>
+          <Text>Data: {notification && JSON.stringify(notification.request.content.data)}</Text>
+        </View>
+        <Button
+          title="Notificação Local"
+          onPress={async () => {
+            await schedulePushNotification();
+          }}
+        />
+    </View>
+  );
+}
+async function NovoCodigo(){
+    number.random(/^(?=.*[a-z])(?=.*[A-Z]){4}$/)
+    await Notifications.scheduleNotificationAsync({
+        content: {
+          title: "Mudança de senha!",
+          body: `Você solicitou uma alteração na sua senha. Para realiza-lá, insira o seguinte código no campo exigido. ${number}`,
+          data: { data: 'goes here' },
+        },
+        trigger: { seconds: 2 },
+      });
+}
+async function navigateToReset() {
+    setLoading(true);
+    if (txtcodigo.trim() === '') {
+        alert('Campo código é obrigatório');
+        return;
+    }
+
+    let resposta = 0;
+    await Notifications.get(`${number}`).then((response) => {
+        //setJokesList(response.data);
+        resposta = response.data.length;
+        
+        if(resposta != Notifications.number){
+            alert('Código inserido invalido!');
+            return;
+        }
+    }).catch(err => alert(err));   
+}
+return (
+    <Formik
+      initialValues={{ txtcodigo: ''}}
+      validationSchema={validationSchema}
+      onSubmit={handleLogin}
+    >
+     {({ handleChange, handleBlur, handleSubmit, values, errors, touched }) => (
+
+        <View style={styles.container}>
+        <Text style={styles.textTitle}>Insira o código que lhe foi enviado!</Text>
+
+
+        <TextInput
+            style={styles.textInput}
+            placeholder="Código"
+            onChangeText={handleChange('Código')}
+            value={values.txtcodigo}
+            onBlur={handleBlur('Código')}
+        />
+        {touched.login && errors.login && <Text style={styles.errorText}>{errors.txtcodigo}</Text>}
+            <Feather
+                style={styles.iconEye}
+                name={iconPass}
+                size={28}
+                color={colors.redButton}
+                onPress={handleChangeIcon}
+            />
+
+    </View>
+    )}
+    </Formik>
+)
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex:1,
+    backgroundColor: '#D93600',
+    alignItems: 'center',
+  },
+  title: {
+    color: '#fff',
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginTop: 50,
+  },
+  inputContainer: {
+    
+    marginTop: 30,
+    width: '90%',
+    padding: 20,
+    borderTopLeftRadius: 10,
+    borderTopRightRadius: 10,
+    borderBottomLeftRadius: 10,
+    borderBottomRightRadius: 10,
+    alignItems: 'stretch',
+    backgroundColor: '#fff',
+    height: '80%'
+
+  },
+  input: {
+    marginTop: 10,
+    height: 60,
+    backgroundColor: '#fff',
+    borderRadius: 10,
+    paddingHorizontal: 24,
+    fontSize: 16,
+    alignItems: 'stretch'
+  },
+  button: {
+    marginTop: 10,
+    height: 60,
+    backgroundColor: 'blue',
+    borderRadius: 10,
+    paddingHorizontal: 24,
+    fontSize: 16,
+    alignItems: 'center',
+    justifyContent: 'center',
+    elevation: 20,
+    shadowOpacity: 20,
+    shadowColor: '#ccc',
+  },
+  buttonText: {
+    color: '#fff',
+    fontWeight: 'bold',
+  },
+  textResult: {
+    color: '#000',
+    fontWeight: 'bold',
+    textAlign : 'center'
+  }
+});
